10c8fcb1e48a86c9419a7e70ea81e90e7fdbc954
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-08-24 21:43:17 -0400

    edited create-resource ejs and linked scss file

--- routes/users.js
+++ routes/users.js
@@ -5,33 +5,33 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const { getUserWithEmail } = require('../db/queries/users');
+const express = require("express");
+const { getUserWithEmail } = require("../db/queries/users");
 const router = express.Router();
-const { addUser } = require('../db/queries/users');
-const { addResource } = require('../db/queries/users');
-const { getUserResources } = require('../db/queries/users');
-const { comment } = require('../db/queries/users');
-const { like, getUser, updateUser } = require('../db/queries/users');
+const { addUser } = require("../db/queries/users");
+const { addResource } = require("../db/queries/users");
+const { getUserResources } = require("../db/queries/users");
+const { comment } = require("../db/queries/users");
+const { like, getUser, updateUser } = require("../db/queries/users");
 
 //homepage
-router.get('/', (req, res) => {
-  res.render('users');
+router.get("/", (req, res) => {
+  res.render("users");
 });
 
 //login page
-router.get('/login', (req, res) => {
-  res.render("login", {user: null});
+router.get("/login", (req, res) => {
+  res.render("login", { user: null });
   // res.status(200).send("ok");
 });
 
 //registration page
-router.get('/register', (req, res) => {
-  res.render('register', {user: null});
+router.get("/register", (req, res) => {
+  res.render("register", { user: null });
 });
 
 //login user
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
   getUserWithEmail(email).then((user) => {
@@ -40,69 +40,66 @@ router.post('/login', (req, res) => {
     }
     req.session.userId = user.id;
     // res.status(200).send("ok");
-    res.redirect('/resources');
+    res.redirect("/resources");
   });
 });
 
 //logout
-router.post('/logout', (req, res) => {
-  res.redirect('/');
+router.post("/logout", (req, res) => {
+  res.redirect("/");
 });
 
 //register new user
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   // if (!req.body.email || !req.body.password) {
   //   res.status(400).send("Please enter a valid email and password!");
   // } else if (emailExists(req.body.email)) {
   //   return res.status(400).send('This email id already registered. Please <a href= "/login" >Login</a>!');
   // } else {
-    const newUser = {
-      userEmail: req.body.email,
-      userPassword: req.body.password,
-      // userID: generateRandomString(),
-    }
-    //function to add data to database
-    addUser(newUser)
-    .then(() => {
-      // req.session.email = newUser.email;
-      // req.session.user_id = id;
-      // res.render('/resources');
-      res.status(200).send("ok");
-    })
+  const newUser = {
+    userEmail: req.body.email,
+    userPassword: req.body.password,
+    // userID: generateRandomString(),
+  };
+  //function to add data to database
+  addUser(newUser).then(() => {
+    // req.session.email = newUser.email;
+    // req.session.user_id = id;
+    // res.render('/resources');
+    res.status(200).send("ok");
+  });
   // }
 });
 
 //New Resource page
-router.get('/resources/new', (req, res) => {
+router.get("/resources/new", (req, res) => {
   // res.status(200).send("ok");
-  res.render('create_resource');
+  res.render("create_resource");
 });
 
-router.post('/resources/new', (req, res) => {
+router.post("/resources/new", (req, res) => {
   const newResource = {
     title: req.body.title,
     description: req.body.description,
     url: req.body.url,
     category: req.body.category,
-  }
+  };
   const user = req.session.email;
   // function to create new resource
-  addResource(newResource, user)
-  .then(() => {
+  addResource(newResource, user).then(() => {
     res.status(200).send("ok");
-  // res.redirect('/resources');
+    // res.redirect('/resources');
   });
 });
 
 //search route
-router.get('/resources/search', (req, res) => {
+router.get("/resources/search", (req, res) => {
   let query = req.query.query;
-  query = query.split(' ');
-  search(query)
-  .then(() => {
-    res.render('resources');
-  })
-})
+  query = query.split(" ");
+  search(query).then(() => {
+    res.render("resources");
+  });
+});
 
 // //resources route to view all resources
 // router.get('/resources', (req, res) => {
@@ -110,68 +107,62 @@ router.get('/resources/search', (req, res) => {
 // })
 
 //view one resource
-router.get('/resources/:id', (req, res) => {
+router.get("/resources/:id", (req, res) => {
   const resourceId = req.params.id;
-  getUserResources(resourceId)
-  .then(() => {
-    res.render('main', { resource: resource, comments: comments });
-  })
+  getUserResources(resourceId).then(() => {
+    res.render("main", { resource: resource, comments: comments });
+  });
 });
 
 //add comment
-router.post('/resources/:id/comment', (req, res) => {
+router.post("/resources/:id/comment", (req, res) => {
   const text = req.body.comment;
   const resourceId = req.params.id;
   const userId = req.session.user_id;
   const rating = req.body.rating;
-  comment(userId, resourceId, text, rating)
-  .then(() => {
-    res.redirect('back');
-  })
+  comment(userId, resourceId, text, rating).then(() => {
+    res.redirect("back");
+  });
 });
 // **********************note sure if likes route is correct**********************************
 //add like
-router.post('/resources/:id/like', (req, res) => {
+router.post("/resources/:id/like", (req, res) => {
   if (!req.session.user_id) {
-    const message = 'Please login to like posts';
-    res.json({message});
+    const message = "Please login to like posts";
+    res.json({ message });
   } else {
     const userId = req.session.user_id;
     const resourceId = req.body.resource_id;
-    like(userId, resourceId)
-    .then(() => {
-      res.redirect('back');
-    })
+    like(userId, resourceId).then(() => {
+      res.redirect("back");
+    });
   }
 });
 
 //user profile page
-router.get('/users/:id', (req, res) => {
+router.get("/users/:id", (req, res) => {
   const currentUser = {
     id: req.session.user_id,
-    user_id: req.params.id
-  }
-  getUser(currentUser)
-  .then(() => {
-    res.status(200).send("ok");
-    // res.render('profile', {};)
-  })
-})
+    user_id: req.params.id,
+  };
+  getUser(currentUser).then(() => {
+    // res.status(200).send("ok");
+    res.render("profile", { user: null });
+  });
+});
 
 //user update profile page
-router.post('/users/:id/update', (req, res) => {
+router.post("/users/:id/update", (req, res) => {
   const updatedUserInfo = {
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-    id: req.session.user_id
-  }
-  updateUser(updatedUserInfo)
-  .then(() => {
-    res.status(200).send("ok");
-    // res.render('profile', {})
+    id: req.session.user_id,
+  };
+  updateUser(updatedUserInfo).then(() => {
+    // res.status(200).send("ok");
+    res.render('profile', {user: null});
   });
-
-})
+});
 
 module.exports = router;
